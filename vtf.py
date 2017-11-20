5c0dd24772b4734c891f51cfa5516c1105a04661
Vinayak Singla <singla.1@iitj.ac.in>
2017-11-20 20:50:50 +0530

    Delete vtf.py

--- vtf.py
+++
@@ -1,9 +0,0 @@
-import subprocess as sp
-
-cmd='ffprobe -i input.avi -show_entries format=duration -v quiet -of csv="p=0"'
-dur=sp.check_output(cmd,shell=True)
-print("dur - ",dur)
-dur=16/float(dur)
-print("dur - ",dur)
-cmd='ffmpeg -i input.avi -vf fps={} out%d.jpg'.format(str(dur))
-sp.call(cmd,shell=True)
